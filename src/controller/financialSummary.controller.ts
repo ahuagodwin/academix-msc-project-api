85589891680d8a3b56549f1fe5c8d355bcdcd5a4
Ahua Godwin <godwincahua@gmail.com>
2025-04-02 21:04:28 +0100

    feat: push updated

--- src/controller/financialSummary.controller.ts
+++ src/controller/financialSummary.controller.ts
@@ -4,8 +4,8 @@ import { Response } from "express";
 import { AuthenticatedRequest, IRole } from "../types/types";
 import { User } from "../models/user.model";
 import { isSystemOwner } from "../middlewares/isSystemOwner";
-import { OutflowAmount } from "../models/outflow.model";
 import { buildQuery, paginate, paginateResults } from "../helpers/Helpers";
+import { OutflowAmount } from "@/models/outflow.model";
 
 export const getFinancialSummary = async (req: AuthenticatedRequest, res: Response): Promise<void> => {
   try {
