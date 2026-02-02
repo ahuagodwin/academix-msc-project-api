56cd097117ae31b89f73fd82089b78b501e1ba73
chidiahua-thc <godwincahua@gmail.com>
2026-02-03 00:07:06 +0100

    staging

--- src/routes/analytics.routes.ts
+++
@@ -1,8 +0,0 @@
-import { authorize, authProtect } from "../middlewares/authorized.md";
-import * as notifyService from "../controller/analytics.controller";
-import { Router } from "express";
-
-const analyticsRouter = Router();
-
-analyticsRouter.get("/analytics/", authProtect, notifyService.getGeneralAnalytics);
-export { analyticsRouter };
--- src/routes/notifications.routes.ts
+++
@@ -1,8 +0,0 @@
-import { authorize, authProtect } from "../middlewares/authorized.md";
-import * as notifyService from "../controller/notification.controller";
-import { Router } from "express";
-
-const notificationRouter = Router();
-
-notificationRouter.get("/notified/", authProtect, authorize("read_notification"), notifyService.getUserNotifications);
-export { notificationRouter };
